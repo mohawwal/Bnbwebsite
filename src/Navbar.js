108317deab6f2ae80931782e73c64048c023b7da
mohawwal <leyeawwal@gmail.com>
2023-02-22 23:45:58 +0100

    added alt to the navbar.js logo image

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar() {
     return(
         <nav>
-            <img src="/images/airbnb-logo.png" className="nav--logo"/>
+            <img src="/images/airbnb-logo.png" alt="logo"  className="nav--logo"/>
         </nav>
     )
 }
