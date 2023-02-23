2ee21e4d9edd29872a67fabe7d537bd9c4534efd
mohawwal <leyeawwal@gmail.com>
2023-02-23 14:03:27 +0100

    cd

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar() {
     return(
         <nav>
-            <img src="/images/airbnb-logo.png" alt="log"  className="nav--logo"/>
+            <img src="/images/airbnb-logo.png" alt="log section"  className="nav--logo"/>
         </nav>
     )
 }
